fc3db3b64b0566d26b783ee3d0a1e4bb7123871f
Michael Ramirez <Michael.Ramirez@Rackspace.com>
2016-11-22 14:28:49 -0600

    convert to webpack

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import * as angular from 'angular';
-import * as _ from 'lodash'
+//import * as _ from 'lodash'
 import home from './home/home.module';
 
 let myApp = angular.module('myApp', [home]);
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  entry: './src/app',
+  output: {
+    filename: './dist/bundle.js'
+  },
+  // Currently we need to add '.ts' to the resolve.extensions array.
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  // Source maps support ('inline-source-map' also works)
+  devtool: 'source-map',
+
+  // Add the loader for .ts files.
+  module: {
+    loaders: [
+      {
+        test: /\.ts$/,
+        loader: 'awesome-typescript-loader'
+      }
+    ]
+  }
+};
