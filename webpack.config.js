deb03ca0ac1ff91f48d9a0fa98cda23a715c1446
Michael Ramirez <Michael.Ramirez@Rackspace.com>
2016-11-25 17:16:34 -0600

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   // Currently we need to add '.ts' to the resolve.extensions array.
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
+    extensions: ['.ts', '.js']
   },
 
   // Source maps support ('inline-source-map' also works)
